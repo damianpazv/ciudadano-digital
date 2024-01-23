d1c187624f2efe9c00ee20f5932d653905fa9d60
unknown <damjpv@gmail.com>
2024-01-23 12:21:11 -0300

    se terminaron los campos y validaciones

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -10,30 +10,41 @@ import logo2 from '../assets/logo_municipalidad.png';
 import logo3 from '../assets/logomuni_piedepagina.png';
 
 export const Registro = () => {
-    const [nombre, setNombre] = useState(''); 
-    const [email, setEmail] = useState('');
-    const [contraseña, setContraseña] = useState('');
+   
     const [confirmarContraseña, setConfirmarContraseña] = useState('');
-    const [telefono, setTelefono] = useState('');
-    const [celular, setCelular] = useState('');
-    const [dni, setDni] = useState('');
-    const [domicilio, setDomicilio] = useState('');
+    
+    const[formData, setFormData]= useState({
+      id_ciudadano:"",
+      dni_ciudadano:"",
+      nombre_ciudadano:"",
+      email_ciudadano:"",
+      clave_ciudadano:"",
+      telefono_ciudadano:"",
+      celular_ciudadano:"",
+      domicilio:"",
+      id_provincia:"",
+      id_localidad:"",
+      validado:"",
+      fecha_carga:"",
+      habilita:""
+    })
    
     const handleRegister = async (e)=>{
         e.preventDefault();
+        
         // Validaciones
         // ! Verificar que no haya campos vacios
-        if(nombre.trim() === '' || telefono === ''  || dni === ''  || domicilio === ''  || celular === '' || email.trim() === '' || contraseña.trim() === '' || confirmarContraseña.trim() === ''){ 
-            return Swal.fire({
-                icon: 'error',
-                title: '¡Ups!',
-                text: 'Todos los campos son obligatorios',                
-              })
-        }
+        // if(nombre.trim() === '' || telefono === ''  || dni === ''  || domicilio === ''  || celular === '' || email.trim() === '' || contraseña.trim() === '' || confirmarContraseña.trim() === ''){ 
+        //     return Swal.fire({
+        //         icon: 'error',
+        //         title: '¡Ups!',
+        //         text: 'Todos los campos son obligatorios',                
+        //       })
+        // }
         // ! Verificar Email
         const patronEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if(!patronEmail.test(email)){
+        if(!patronEmail.test(formData.email_ciudadano)){
             return Swal.fire({
                 icon: 'error',
                 title: '¡Ups!',
@@ -41,14 +52,67 @@ export const Registro = () => {
               })
         }
         // ! Verificar que las contraseñas sean iguales
-        if( contraseña !== confirmarContraseña){
+        if( formData.clave_ciudadano !== confirmarContraseña){
             return Swal.fire({
                 icon: 'error',
                 title: '¡Ups!',
-                text: 'Las contraseñas no coinciden',                
+                text: 'Las claves no coinciden',                
               })
         }
-      
+
+        if( formData.dni_ciudadano.length != 8){
+          return Swal.fire({
+              icon: 'error',
+              title: '¡Ups!',
+              text: 'El DNI debe tener 8 caracteres',                
+            })
+      }
+
+      if( formData.celular_ciudadano.length != 10){
+        return Swal.fire({
+            icon: 'error',
+            title: '¡Ups!',
+            text: 'El nro de celular debe tener 10 caracteres sin guiones ejemplo: 3814123456',                
+          })
+    }
+
+    if( formData.telefono_ciudadano.length != 7){
+      return Swal.fire({
+          icon: 'error',
+          title: '¡Ups!',
+          text: 'El nro de teléfono debe tener 7 caracteres sin guiones ejemplo: 4223456',                
+        })
+  }
+
+  if( formData.id_provincia == 0){
+    return Swal.fire({
+        icon: 'error',
+        title: '¡Ups!',
+        text: 'Debe seleccionar una provincia',                
+      })
+}
+if( formData.id_localidad == 0){
+  return Swal.fire({
+      icon: 'error',
+      title: '¡Ups!',
+      text: 'Debe seleccionar una localidad',                
+    })
+}
+
+
+  setFormData({
+    ...formData,
+    fecha_carga: new Date().toLocaleString(),
+});
+  console.log(formData)
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "El formulario de registro se envió correctamente, le enviaremos un email de validación para finalizar el registro",
+    showConfirmButton: false,
+    timer: 2500
+  });
+
      
         // ! Creo usuario en la base de datos
 
@@ -100,6 +164,27 @@ export const Registro = () => {
 
         
     }
+
+    const handleChange = (e,lon) => {
+
+if(e.target.type=="number")
+{
+  setFormData({
+    ...formData,
+    [e.target.name]: e.target.value.slice(0,lon),
+});
+}
+else{
+  setFormData({
+    ...formData,
+    [e.target.name]: e.target.value,
+});
+}
+     
+       }
+
+
+
   return (
     <>
    
@@ -130,21 +215,24 @@ export const Registro = () => {
       type="text"
       placeholder="Juan Perez"
       name='nombre_ciudadano'
-      onChange={(e) => setNombre(e.target.value)}
+      onChange={handleChange}
       maxLength={50}
+      minLength={5}
       required
+      value={formData.nombre_ciudadano}
     />
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="email">
-    <Form.Label> <strong>Email address</strong> </Form.Label>
+    <Form.Label> <strong>Email </strong> </Form.Label>
     <Form.Control
       type="email"
       placeholder="name@example.com"
       name="email_ciudadano"
-      onChange={(e) => setEmail(e.target.value)}
+      onChange={handleChange}
       maxLength={70}
       required
+      value={formData.email_ciudadano}
     />
   </Form.Group>
 
@@ -154,9 +242,10 @@ export const Registro = () => {
       type="number"
       placeholder="4223456"
       name="telefono_ciudadano"
-      onChange={(e) => setTelefono(e.target.value)}
-      maxLength={15}
+      onChange={(e)=>handleChange(e,7)}
+      value={formData.telefono_ciudadano}
       required
+      
     />
   </Form.Group>
 
@@ -166,32 +255,33 @@ export const Registro = () => {
       type="number"
       placeholder="3813456789"
       name="celular_ciudadano"
-      onChange={(e) => setCelular(e.target.value)}
-      maxLength={15}
+      onChange={(e)=>handleChange(e,10)}
+      value={formData.celular_ciudadano}
       required
     />
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="contraseña">
-    <Form.Label> <strong>Contraseña</strong> </Form.Label>
+    <Form.Label> <strong>Clave</strong> </Form.Label>
     <Form.Control
       type="password"
-      placeholder='Escriba una contraseña'
+      placeholder='Escriba una clave'
       name="clave_ciudadano"
-      onChange={(e) => setContraseña(e.target.value)}
-      minLength={8}
+      onChange={handleChange}
+      value={formData.clave_ciudadano}
+      minLength={15}
       maxLength={15}
       required
     />
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="confirmarContraseña">
-    <Form.Label> <strong>Confirmar Contraseña</strong> </Form.Label>
+    <Form.Label> <strong>Confirmar Clave</strong> </Form.Label>
     <Form.Control
       type="password"
-      placeholder='Repetir contraseña'
+      placeholder='Repetir clave'
       onChange={(e) => setConfirmarContraseña(e.target.value)}
-      minLength={8}
+      minLength={15}
       maxLength={15}
       required
     />
@@ -202,10 +292,11 @@ export const Registro = () => {
     <Form.Control
       type="number"
       placeholder='16234568'
-      onChange={(e) => setDni(e.target.value)}
+      onChange={(e)=>handleChange(e,8)}
+      value={formData.dni_ciudadano}
       name="dni_ciudadano"
-      maxLength={10}
       required
+      
     />
   </Form.Group>
 
@@ -214,12 +305,55 @@ export const Registro = () => {
     <Form.Control
       type="text"
       placeholder='Mendoza 345'
-      onChange={(e) => setDomicilio(e.target.value)}
+      onChange={handleChange}
+      value={formData.domicilio}
       name="domicilio"
       maxLength={100}
       required
     />
   </Form.Group>
+
+  <Form.Group className="mb-3" controlId="dni">
+    <Form.Label> <strong>Provincia</strong> </Form.Label>
+    
+ <Form.Select 
+ type="number"    
+ onChange={handleChange}
+ value={formData.id_provincia}
+ name="id_provincia"
+ maxLength={2}
+ required
+ 
+ >
+      <option value={0}>Seleccione Provincia</option>
+      <option value={11} >Tucuman</option>
+      <option value={22}>Salta</option>
+      <option value={33}>Jujuy</option>
+      
+    </Form.Select>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="dni">
+    <Form.Label> <strong>Provincia</strong> </Form.Label>
+    
+ <Form.Select 
+ type="number"    
+ onChange={handleChange}
+ value={formData.id_localidad}
+ name="id_localidad"
+ 
+ required
+ 
+ >
+      <option value={0}>Seleccione Localidad</option>
+      <option value={1111} >San miguel de Tucuman</option>
+      <option value={2222}>San Cayetano</option>
+      <option value={3333}>San Jose</option>
+      
+    </Form.Select>
+  </Form.Group>
+
+ 
 
   <div className='className="d-grid gap-2"'>
   <Button size='lg' variant="primary" type="submit" className="w-100">
